fb0bc9fb84c2d15b5801c57ff9d48fa58057cbfb
Nathan Hjelm <hjelmn@me.com>
2024-02-25 20:58:06 -0700

    Add license to main.c
    This is under LGPLv2.
    
    Signed-off-by: Nathan Hjelm <hjelmn@me.com>

--- ignignokt.X/main.c
+++ ignignokt.X/main.c
@@ -2,6 +2,22 @@
  * File:   main.c
  * Author: hjelmn
  *
+ * (c) 2024      Nathan Hjelm <hjelmn@mac.com>
+ *
+ * This binary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
  * Created on February 25, 2024, 8:53 AM
  */
 
